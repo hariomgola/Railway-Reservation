6a0cad7b67ea37264432a8383636d18ed73b3e9f
hariom <hariom987316@gmail.com>
2024-01-14 23:15:59 +0530

    adding deployment dependencies

---
+++ notes.ts
@@ -0,0 +1,6 @@
+/**
+  # Deploy Angular Application to gn-pages
+    - npm i angular-cli-ghpages --save-dev
+    - ng build --base-href "https://hariomgola.github.io/Railway-Reservation/"
+    - npx angular-cli-ghpages --dir=dist/Railway-Reservation  
+ */
